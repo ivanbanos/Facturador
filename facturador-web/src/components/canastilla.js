c2525e0c3f79e231da5aec3e4fb2a14499c11b58
karenvicent <karenvergara@msn.com>
2023-09-28 10:31:09 -0500

    [WEB] agregar  vista combustible, header y routes

---
+++ facturador-web/src/components/canastilla.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/home.css";
+
+const Canastilla = () => {
+  return (
+    <>
+      <div className="pt-4 pb-4 left-column columnas w-100 h-100">
+        <h1 className="text-white">Canastilla</h1>
+      </div>
+    </>
+  );
+};
+
+export default Canastilla;
---
+++ facturador-web/src/components/terceros.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/home.css";
+
+const Terceros = () => {
+  return (
+    <>
+      <h1>Terceros</h1>
+    </>
+  );
+};
+
+export default Terceros;
